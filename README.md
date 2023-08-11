c887b9d81785a5384b75fc911cd369f6c56848c1
Siah Wei Yu <siahweiyu0603@gmail.com>
2023-08-12 02:00:36 +0800

    dynamic inputs working??

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 River Crossing Problem Simulation using Web Animations API
 
 # Weekly Tasks
-1. Build grids in HTML page (Done!)
-2. Find out how to dynamically create animals (Done!)
-3. Create function to randomise animal count (Done!)
-4. Create function to dynamically create animals (Done!)
+1. Change start and end positions to be generalised (animal1-start, animal2-start, animal1-end, animal2-end) (Done!)
+2. Think of how to move animals
 
-Will need to programtically assign animals and their start positions unique IDs
+Create 2 arrays? For each side? (Done!)
 
-E.g.
-g1-start for position
-g1 for goat
+Loop through to see if there is a goat or lion occupying the space? (If want to find start animal to move) (Done!)
+If found animal, get position and move
+
+Loop through to find next empty space (If want to find end position for animal to move to) (Done!)
+If found empty space, get position and move
+
+Need to change IS1 codes? Return an array of the corresponding moves?
+OR, find out how to find the corresponding moves from the IS1 program results
+
+Found!
+From final results, take deduct to find the animals move.
+If current is L, calculate for first two numbers, if R, calculate for last two
+E.g. 
+L5400 -> R4311 (5 - 4 = 1 Goat Moved, 4 - 3 = 1 Lion Moved)
+R4311 -> L5301 (1 - 0 = 1 Goat Moved, 1 - 1 = 0 Lion Moved)
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    // const goat1 = document.querySelector('#goat1');
-    // const lion1 = document.querySelector('#lion1');
-    // const goat1End = document.querySelector('#goat1-end');
-    // const lion1End = document.querySelector('#lion1-end');
     const raft = document.querySelector('#raft');
     const animalStart = document.querySelector('#animal-start');
     const animalEnd = document.querySelector("#animal-end");
@@ -17,6 +13,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const startGrid = document.querySelector('#left-grid');
     const endGrid = document.querySelector('#right-grid');
 
+    const leftBank = [];
+    const rightBank = [];
+
+    var numGoat;
+    var numLion;
+
     //Resources:
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
     // https://developer.mozilla.org/en-US/docs/Web/API/DOMRect
@@ -24,27 +26,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
     btnStart.onclick = function () {
         generateAnimals();
-        // move(goat, animalStart)
-        //     .then(() => move(goat, raftStartTop))
-        //     .then(() => move(goat, raftEndTop))
-        //     .then(() => move(goat, animalEnd));
+        var animal1 = getStartAnimal('G', 'L'); // animal, bank
+        var endPos1 = getEndPosition(animal1, 'L'); // new animal, bank
 
-        const goat1 = document.querySelector('#G1');
-        const lion1 = document.querySelector('#L1');
-        const goat1End = document.querySelector('#G1-end');
-        const lion1End = document.querySelector('#L1-end');
+        var animal2 = getStartAnimal('L', 'L');
+        var endPos2 = getEndPosition(animal2, 'L');
 
-        move(goat1, animalStart, 1000)
-            .then(() => move(goat1, raftStartTop, 1000))
-            .then(() => move(lion1, animalStart, 1000))
-            .then(() => move(lion1, raftStartBottom, 1000))
+        var idAnimal1 = '#' + animal1;
+        var idAnimal2 = '#' + animal2;
+        var idEndPos1 = '#' + endPos1;
+        var idEndPos2 = '#' + endPos2;
+
+        const a1 = document.querySelector(idAnimal1);
+        const a2 = document.querySelector(idAnimal2);
+        const a1End = document.querySelector(idEndPos1);
+        const a2End = document.querySelector(idEndPos2);
+
+        var animal3 = getStartAnimal('G', 'R'); // animal, bank
+        var endPos3 = getEndPosition(animal3, 'R'); // new animal, bank
+
+        var animal4 = getStartAnimal('L', 'R');
+        var endPos4 = getEndPosition(animal4, 'R');
+
+        var idAnimal3 = '#' + animal3;
+        var idAnimal4 = '#' + animal4;
+        var idEndPos3 = '#' + endPos3;
+        var idEndPos4 = '#' + endPos4;
+
+        const a3 = document.querySelector(idAnimal3);
+        const a4 = document.querySelector(idAnimal4);
+        const a3End = document.querySelector(idEndPos3);
+        const a4End = document.querySelector(idEndPos4);
+
+        // move(a1, animalStart, 1000)
+        //     .then(() => move(a1, raftStartTop, 1000))
+        //     .then(() => move(a2, animalStart, 1000))
+        //     .then(() => move(a2, raftStartBottom, 1000))
+        //     .then(() => {
+        //         return Promise.all([move(a1, raftEndTop), move(a2, raftEndBottom), move(raft, raftEnd)])
+        //     })
+        //     .then(() => move(a1, animalEnd, 1000))
+        //     .then(() => move(a1, a1End, 1000))
+        //     .then(() => move(a2, animalEnd, 1000))
+        //     .then(() => move(a2, a2End, 1000));
+
+        move(a1, animalStart, 1000)
+            .then(() => move(a1, raftStartTop, 1000))
+            .then(() => move(a2, animalStart, 1000))
+            .then(() => move(a2, raftStartBottom, 1000))
             .then(() => {
-                return Promise.all([move(goat1, raftEndTop), move(lion1, raftEndBottom), move(raft, raftEnd)])
+                return Promise.all([move(a1, raftEndTop), move(a2, raftEndBottom), move(raft, raftEnd)])
             })
-            .then(() => move(goat1, animalEnd, 1000))
-            .then(() => move(goat1, goat1End, 1000))
-            .then(() => move(lion1, animalEnd, 1000))
-            .then(() => move(lion1, lion1End, 1000));
+            .then(() => move(a1, animalEnd, 1000))
+            .then(() => move(a1, a1End, 1000))
+            .then(() => move(a2, animalEnd, 1000))
+            .then(() => move(a2, a2End, 1000))
+            .then(() => move(a3, animalEnd, 1000))
+            .then(() => move(a3, raftEndTop, 1000))
+            .then(() => move(a4, animalEnd, 1000))
+            .then(() => move(a4, raftEndBottom, 1000))
+            .then(() => {
+                return Promise.all([move(a3, raftStartTop), move(a4, raftStartBottom), move(raft, raftStart)])
+            })
+            .then(() => move(a3, animalStart, 1000))
+            .then(() => move(a3, a3End, 1000))
+            .then(() => move(a4, animalStart, 1000))
+            .then(() => move(a4, a4End, 1000));
     }
 
     // translate happens from the original position
@@ -53,9 +100,6 @@ window.addEventListener('DOMContentLoaded', () => {
         // get start and end coordinates
         const startPos = getPosition(startElement);
         const endPos = getPosition(endElement);
-
-        // console.log("startPos: ", startPos);
-        // console.log("endPos: ", endPos);
 
         // get distance to travel
         const dist = calcDistance(startPos, endPos);
@@ -66,8 +110,6 @@ window.addEventListener('DOMContentLoaded', () => {
         const moveAnimation = [
             { transform: `translate(${dist.x + distCurrent.x}px, ${dist.y + distCurrent.y}px)` }
         ];
-
-        // console.log(`{ transform: translate(${dist.x + distCurrent.x}px, ${dist.y + distCurrent.y}px) }`)
 
         const moveTiming = {
             duration: time,
@@ -113,35 +155,45 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function generateAnimals() {
+        const animal = 'A';
         const goat = 'G';
         const lion = 'L';
         const start = 'start';
         const end = 'end';
-        const numGoat = Math.floor(Math.random() * (8 - 1 + 1) + 1);
-        const numLion = Math.floor(Math.random() * (numGoat - 1 + 1) + 1); // lions must never start higher than goats
+        numGoat = Math.floor(Math.random() * (8 - 1 + 1) + 1);
+        numLion = Math.floor(Math.random() * (numGoat - 1 + 1) + 1); // lions must never start higher than goats
 
         console.log(numGoat);
         console.log(numLion);
 
         for (let i = 0; i < numGoat; i++) {
-            const goatStartElement = createAnimalContainer(goat, i + 1, start);
+            const goatStartElement = createAnimalContainer(animal, i + 1, start);
             startGrid.appendChild(goatStartElement);
             const goatElement = createAnimals(goat, i + 1);
             goatStartElement.appendChild(goatElement);
 
-            const goatEndElement = createAnimalContainer(goat, i + 1, end);
+            const goatEndElement = createAnimalContainer(animal, i + 1, end);
             endGrid.appendChild(goatEndElement);
+
+            leftBank.push(goat + (i + 1));
+            rightBank.push(null);
         }
 
         for (let i = 0; i < numLion; i++) {
-            const lionStartElement = createAnimalContainer(lion, i + 1, start);
+            const lionStartElement = createAnimalContainer(animal, i + 1 + numGoat, start);
             startGrid.appendChild(lionStartElement);
             const lionElement = createAnimals(lion, i + 1);
             lionStartElement.appendChild(lionElement);
 
-            const lionEndElement = createAnimalContainer(lion, i + 1, end);
+            const lionEndElement = createAnimalContainer(animal, i + 1 + numGoat, end);
             endGrid.appendChild(lionEndElement);
+
+            leftBank.push(lion + (i + 1));
+            rightBank.push(null);
         }
+
+        console.log(leftBank)
+        console.log(rightBank)
     }
 
     function createAnimalContainer(animal, index, pos) {
@@ -156,5 +208,66 @@ window.addEventListener('DOMContentLoaded', () => {
         animalElement.id = animal + index;
         animalElement.innerHTML = animal == 'G' ? '&#x1F984' : '&#x1F981';
         return animalElement;
+    }
+
+    // get the first available animal in respective array
+    // bank refers to current raft position : LEFT or RIGHT
+    // also empties the selected animal position in the current bank
+    function getStartAnimal(animal, bank) {
+        var animalID;
+
+        if (bank == 'L') {
+            for (let i = 0; i < leftBank.length; i++) {
+                if (leftBank[i] != null && leftBank[i].charAt(0) == animal) {
+                    console.log('Found animal for leftBank at index: ' + i)
+                    animalID = leftBank[i];
+                    leftBank[i] = null; // empty the value
+                    return animalID; // return index of selected animal
+                }
+            }
+        } else if (bank == 'R') {
+            for (let i = 0; i < rightBank.length; i++) {
+                if (rightBank[i] != null && rightBank[i].charAt(0) == animal) {
+                    console.log('Found animal for rightBank at index: ' + i)
+                    animalID = rightBank[i];
+                    rightBank[i] = null; // empty the value
+                    return animalID;
+                }
+            }
+        }
+
+        console.log('Impossible move found!');
+        return null;
+    }
+
+    // get the first available empty space in respective array
+    // bank refers to current raft position : LEFT or RIGHT
+    // also populates the opposite bank with the corresponding new animal
+    function getEndPosition(animal, bank) {
+        var animalPos;
+
+        // if currently on right, end position is left
+        if (bank == 'R') {
+            for (let i = 0; i < leftBank.length; i++) {
+                if (leftBank[i] == null) {
+                    console.log('Found empty for leftBank at index: ' + i)
+                    animalPos = 'A' + (i + 1) + '-start';
+                    leftBank[i] = animal; // populate with new animal
+                    return animalPos; // return index of selected animal
+                }
+            }
+        } else if (bank == 'L') { // if currently on left, end position is right
+            for (let i = 0; i < rightBank.length; i++) {
+                if (rightBank[i] == null) {
+                    console.log('Found empty for rightBank at index: ' + i)
+                    animalPos = 'A' + (i + 1) + '-end';
+                    rightBank[i] = animal; // populate with new animal
+                    return animalPos;
+                }
+            }
+        }
+
+        console.log('Impossible move found!');
+        return null;
     }
 });
