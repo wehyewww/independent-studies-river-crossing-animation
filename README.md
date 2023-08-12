e2ddb6f9efa4c9b5475592c201c4c1ee2b3fcadf
Siah Wei Yu <siahweiyu0603@gmail.com>
2023-08-13 00:05:50 +0800

    playback for speed, pause, resume

--- README.md
+++ README.md
@@ -2,27 +2,4 @@
 River Crossing Problem Simulation using Web Animations API
 
 # Weekly Tasks
-1. Add River Crossing Solver codes into application (Done!)
-2. Create function to get start animals and end positions, get element based of HTML ID, and call respective moves? (Done!)
-3. Consider how to handle situations where 1 animal moving vs 2 animal moving (Done!)
-4. Consider order of program (Done!)
--> Click button, generate number of goats and lions
--> Call solver and receive results
--> Calculate Goat and Lion moved for first move, call function to get animal and position
--> Call move functions (Will need to chain promises together? Finish 1 set of animation, do calculations, start next animation?)
-
-Better to have function to do 1 step at a time
-
--> Looking forward into playback, would be good to keep array of steps
--> User can skip or reverse to previous step
--> This way, program will always know what comes next
-
-Need to change IS1 codes? Return an array of the corresponding moves?
-OR, find out how to find the corresponding moves from the IS1 program results
-
-Found!
-From final results, take deduct to find the animals move.
-If current is L, calculate for first two numbers, if R, calculate for last two
-E.g. 
-L5400 -> R4311 (5 - 4 = 1 Goat Moved, 4 - 3 = 1 Lion Moved)
-R4311 -> L5301 (1 - 0 = 1 Goat Moved, 1 - 1 = 0 Lion Moved)
+1. Playback (Speed up, Slow down, pause, resume) (Done!)
--- index.js
+++ index.js
@@ -12,6 +12,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const raftEndTop = document.querySelector('#raft-end-top');
     const raftEndBottom = document.querySelector('#raft-end-bottom');
     const btnStart = document.querySelector('#btn-start');
+    const btnSpeedUp = document.querySelector('#btn-speed-up');
+    const btnSlowDown = document.querySelector('#btn-slow-down');
+    const btnPause = document.querySelector('#btn-pause');
+    const btnResume = document.querySelector('#btn-resume');
     const startGrid = document.querySelector('#left-grid');
     const endGrid = document.querySelector('#right-grid');
 
@@ -20,6 +24,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     var numGoat;
     var numLion;
+    var animationSpeedFactor = 1; // playback rate
+    var ongoingAnimations = [];
 
     //Resources:
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
@@ -33,88 +39,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if (states != null) {
             runAllStages(states);
-
-            // var animalMove = getSteps('L3300', 'R2211');
-            // runStage(animalMove.goat, animalMove.lion, animalMove.raft);
-
-            // var animalMove = getSteps('R2211', 'L3201');
-            // runStage(animalMove.goat, animalMove.lion, animalMove.raft);
-
-            // var animal1 = getStartAnimal('G', 'L'); // animal, bank
-            // var endPos1 = getEndPosition(animal1, 'L'); // new animal, bank
-
-            // var animal2 = getStartAnimal('L', 'L');
-            // var endPos2 = getEndPosition(animal2, 'L');
-
-            // var idAnimal1 = '#' + animal1;
-            // var idAnimal2 = '#' + animal2;
-            // var idEndPos1 = '#' + endPos1;
-            // var idEndPos2 = '#' + endPos2;
-
-            // const a1 = document.querySelector(idAnimal1);
-            // const a2 = document.querySelector(idAnimal2);
-            // const a1End = document.querySelector(idEndPos1);
-            // const a2End = document.querySelector(idEndPos2);
-
-            // // testing
-            // var animal3 = getStartAnimal('G', 'R'); // animal, bank
-            // var endPos3 = getEndPosition(animal3, 'R'); // new animal, bank
-
-            // var animal4 = getStartAnimal('L', 'R');
-            // var endPos4 = getEndPosition(animal4, 'R');
-
-            // var idAnimal3 = '#' + animal3;
-            // var idAnimal4 = '#' + animal4;
-            // var idEndPos3 = '#' + endPos3;
-            // var idEndPos4 = '#' + endPos4;
-
-            // const a3 = document.querySelector(idAnimal3);
-            // const a4 = document.querySelector(idAnimal4);
-            // const a3End = document.querySelector(idEndPos3);
-            // const a4End = document.querySelector(idEndPos4);
-
-            // move(a1, animalStart, 1000)
-            //     .then(() => move(a1, raftStartTop, 1000))
-            //     .then(() => move(a2, animalStart, 1000))
-            //     .then(() => move(a2, raftStartBottom, 1000))
-            //     .then(() => {
-            //         return Promise.all([move(a1, raftEndTop), move(a2, raftEndBottom), move(raft, raftEnd)])
-            //     })
-            //     .then(() => move(a1, animalEnd, 1000))
-            //     .then(() => move(a1, a1End, 1000))
-            //     .then(() => move(a2, animalEnd, 1000))
-            //     .then(() => move(a2, a2End, 1000));
-
-            // move(a1, animalStart, 1000)
-            //     .then(() => move(a1, raftStartTop, 1000))
-            //     .then(() => move(a2, animalStart, 1000))
-            //     .then(() => move(a2, raftStartBottom, 1000))
-            //     .then(() => {
-            //         return Promise.all([move(a1, raftEndTop), move(a2, raftEndBottom), move(raft, raftEnd)])
-            //     })
-            //     .then(() => move(a1, animalEnd, 1000))
-            //     .then(() => move(a1, a1End, 1000))
-            //     .then(() => move(a2, animalEnd, 1000))
-            //     .then(() => move(a2, a2End, 1000))
-            //     .then(() => move(a3, animalEnd, 1000))
-            //     .then(() => move(a3, raftEndTop, 1000))
-            //     .then(() => move(a4, animalEnd, 1000))
-            //     .then(() => move(a4, raftEndBottom, 1000))
-            //     .then(() => {
-            //         return Promise.all([move(a3, raftStartTop), move(a4, raftStartBottom), move(raft, raftStart)])
-            //     })
-            //     .then(() => move(a3, animalStart, 1000))
-            //     .then(() => move(a3, a3End, 1000))
-            //     .then(() => move(a4, animalStart, 1000))
-            //     .then(() => move(a4, a4End, 1000));
         } else {
             console.log('No solution found!')
         }
+    }
 
+    btnSpeedUp.onclick = function () {
+        updateAnimationSpeed(2);
+    }
+
+    btnSlowDown.onclick = function () {
+        updateAnimationSpeed(0.5);
+    }
+
+    btnPause.onclick = function () {
+        pauseAnimation();
+    }
+
+    btnResume.onclick = function () {
+        resumeAnimation();
     }
 
     // translate happens from the original position
-    function move(startElement, endElement, time = 2000) {
+    function move(startElement, endElement, time, rate) {
 
         // get start and end coordinates
         const startPos = getPosition(startElement);
@@ -136,6 +83,10 @@ window.addEventListener('DOMContentLoaded', () => {
         };
 
         const animation = startElement.animate(moveAnimation, moveTiming);
+
+        animation.updatePlaybackRate(animationSpeedFactor);
+
+        ongoingAnimations.push(animation);
 
         return animation.finished;
     }
@@ -178,8 +129,6 @@ window.addEventListener('DOMContentLoaded', () => {
         numGoat = Math.floor(Math.random() * (8 - 1 + 1) + 1);
         numLion = Math.floor(Math.random() * (numGoat - 1 + 1) + 1); // lions must never start higher than goats
 
-        console.log(numGoat);
-        console.log(numLion);
         return { goat: numGoat, lion: numLion }
     }
 
@@ -275,7 +224,6 @@ window.addEventListener('DOMContentLoaded', () => {
         if (bank == 'R') {
             for (let i = 0; i < leftBank.length; i++) {
                 if (leftBank[i] == null) {
-                    console.log('Found empty for leftBank at index: ' + i)
                     animalPos = 'A' + (i + 1) + '-start';
                     leftBank[i] = animal; // populate with new animal
                     return animalPos; // return index of selected animal
@@ -284,7 +232,6 @@ window.addEventListener('DOMContentLoaded', () => {
         } else if (bank == 'L') { // if currently on left, end position is right
             for (let i = 0; i < rightBank.length; i++) {
                 if (rightBank[i] == null) {
-                    console.log('Found empty for rightBank at index: ' + i)
                     animalPos = 'A' + (i + 1) + '-end';
                     rightBank[i] = animal; // populate with new animal
                     return animalPos;
@@ -301,6 +248,8 @@ window.addEventListener('DOMContentLoaded', () => {
         var startAnimal2;
 
         let currentPromise = Promise.resolve();
+
+        ongoingAnimations = []; // reset ongoing animations
 
         if (goat + lion == 2) {
 
@@ -338,7 +287,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     .then(() => move(a2, animalStart, 1000))
                     .then(() => move(a2, raftStartBottom, 1000))
                     .then(() => {
-                        return Promise.all([move(a1, raftEndTop), move(a2, raftEndBottom), move(raft, raftEnd)])
+                        return Promise.all([move(a1, raftEndTop, 2000), move(a2, raftEndBottom, 2000), move(raft, raftEnd, 2000)])
                     })
                     .then(() => move(a1, animalEnd, 1000))
                     .then(() => move(a1, a1End, 1000))
@@ -351,7 +300,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     .then(() => move(a2, animalEnd, 1000))
                     .then(() => move(a2, raftEndBottom, 1000))
                     .then(() => {
-                        return Promise.all([move(a1, raftStartTop), move(a2, raftStartBottom), move(raft, raftStart)])
+                        return Promise.all([move(a1, raftStartTop, 2000), move(a2, raftStartBottom, 2000), move(raft, raftStart, 2000)])
                     })
                     .then(() => move(a1, animalStart, 1000))
                     .then(() => move(a1, a1End, 1000))
@@ -381,7 +330,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     .then(() => move(a1, animalStart, 1000))
                     .then(() => move(a1, raftStartTop, 1000))
                     .then(() => {
-                        return Promise.all([move(a1, raftEndTop), move(raft, raftEnd)])
+                        return Promise.all([move(a1, raftEndTop, 2000), move(raft, raftEnd, 2000)])
                     })
                     .then(() => move(a1, animalEnd, 1000))
                     .then(() => move(a1, a1End, 1000));
@@ -390,7 +339,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     .then(() => move(a1, animalEnd, 1000))
                     .then(() => move(a1, raftEndTop, 1000))
                     .then(() => {
-                        return Promise.all([move(a1, raftStartTop), move(raft, raftStart)])
+                        return Promise.all([move(a1, raftStartTop, 2000), move(raft, raftStart, 2000)])
                     })
                     .then(() => move(a1, animalStart, 1000))
                     .then(() => move(a1, a1End, 1000));
@@ -427,5 +376,31 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         return { goat: goatMoved, lion: lionMoved, raft: raftPos }
+    }
+
+    // function to update animation speed
+    function updateAnimationSpeed(speedFactor) {
+
+        animationSpeedFactor *= speedFactor;
+
+        ongoingAnimations.forEach(animation => {
+            animation.updatePlaybackRate(animationSpeedFactor);
+        });
+    }
+
+    function pauseAnimation() {
+        ongoingAnimations.forEach(animation => {
+            if (animation.playState == 'running') {
+                animation.pause()
+            }
+        })
+    }
+
+    function resumeAnimation() {
+        ongoingAnimations.forEach(animation => {
+            if (animation.playState == 'paused') {
+                animation.play()
+            }
+        })
     }
 });
